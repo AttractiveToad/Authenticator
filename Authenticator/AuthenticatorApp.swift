2ed5aa6818acb8bd8a9d04c06daad458c3a12d94
AttractiveToad <alois.zimmermann2003@gmail.com>
2022-01-07 22:14:33 +0100

    Initial Commit

---
+++ Authenticator/AuthenticatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticatorApp.swift
+//  Authenticator
+//
+//  Created by Alois Zimmermann on 07.01.22.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
